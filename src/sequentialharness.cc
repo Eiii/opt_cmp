5b6f5781de4417213ddfd0e73c36d3214ba8e17f
Erich Merrill <erich@erichmerrill.com>
2016-10-20 23:16:05 -0700

    Added basic exception handling to BO harness

--- src/sequentialharness.cc
+++ src/sequentialharness.cc
@@ -16,11 +16,15 @@ SequentialHarness::SequentialHarness(std::string name, const Function& fn,
 void SequentialHarness::Evaluate(int max_samples, int iterations)
 {
   for (int i = 0; i < iterations; i++) {
-    RandomInt rand_seed(rng_, UniformIntDist(0, std::numeric_limits<int>::max()));
-    SingleRun(rand_seed(), max_samples);
-    if ((i+1) % 5 == 0) {
-      std::cout << "Finished iteration " << (i+1) << std::endl;
-      std::cout.flush();
+    try {
+      RandomInt rand_seed(rng_, UniformIntDist(0, std::numeric_limits<int>::max()));
+      SingleRun(rand_seed(), max_samples);
+      if ((i+1) % 5 == 0) {
+        std::cout << "Finished iteration " << (i+1) << std::endl;
+        std::cout.flush();
+      }
+    } catch (const std::exception& e) {
+      std::cout << "EXCEPTION @ " << i << ": " << e.what() << std::endl;
     }
   }
 } /* Evaluate() */
